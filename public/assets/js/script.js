4700d2d93c0843c0752515d03657d064d9474801
tanakakeishiro <tanakakeishiro349489@gmail.com>
2025-12-04 12:59:16 +0900

    refactor(index, style): BEM命名規則に基づくクラス名の変更
    - index.html と style.css のクラス名を BEM に従って修正
    - .fv-content を .fv__content に変更
    - .fv-content__image を .fv__image に変更
    - その他関連するクラス名も同様に修正
    - コードの可読性を向上させるための変更

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -27,14 +27,16 @@
   switchViewport();
 })();
 
+
 jQuery("#js-drawer-button").on("click", function (e) {
   e.preventDefault();
   jQuery("#js-drawer-button").toggleClass("is-checked");
   jQuery("#js-drawer-content").toggleClass("is-checked");
 });
 
-// sp表示のときにドロワーメニューが開いている状態でリンクをクリックしたときに、ドロワーメニューを閉じるようにするためのコードです。
+// sp表示のときにドロワーメニューが開いている状態でリンクをクリックしたときに、ドロワーメニューを閉じるようにするためのコード。
 jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
   jQuery("#js-drawer-icon").removeClass("is-checked");
   jQuery("#js-drawer-content").removeClass("is-checked");
 });
+
